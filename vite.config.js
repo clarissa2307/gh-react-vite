63c605b61554604dd2cbea0b263a532d883f174d
clarissa2307 <Marthaleo07@gmail.com>
2022-08-19 10:44:59 -0500

    Fix Bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://clarissa2307.github.io/',
+  base: 'https://clarissa2307.github.io/gh-react-vite/',
   plugins: [react()]
 })
